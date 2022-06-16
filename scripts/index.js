a27732145a0aba452ca2ed93553db8441b468862
Yulia Burenko <juliaburenko@yandex.ru>
2022-06-16 22:10:35 +0300

    feat: animated popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,29 @@
 const popup = document.querySelector('.popup');
-const form = popup.querySelector('.popup__form');
+const popupForm = popup.querySelector('.popup__form');
 const closingButton = popup.querySelector('.popup__close-button');
-const accountButton = document.getElementById('account');
+const accountButton = document.querySelector('.account');
 
-function openPopup(popup) {
+function openPopup() {
   popup.classList.add('popup_active');
   document.addEventListener("keydown", handleEscPress);
 };
 
-function closePopup (popup) {
+function closePopup () {
   popup.classList.remove('popup_active');
+  popupForm.reset();
   document.removeEventListener("keydown", handleEscPress);
 };
 
 accountButton.addEventListener('click',
   openPopup);
+
 closingButton.addEventListener('click',
   closePopup);
 
 const handleEscPress = (evt) => {
   if (evt.key === "Escape") {
     const popupOpened = document.querySelector(".popup_active");
-    closePopup(popupOpened);
+    closePopup();
   }
 };
 
