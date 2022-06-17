b261aedc198fb9eabc582d87b2cfd2392ed19a19
Yulia Burenko <juliaburenko@yandex.ru>
2022-06-17 17:21:37 +0300

    feat: added slides for quotes section

--- scripts/index.js
+++ scripts/index.js
@@ -27,7 +27,35 @@ const handleEscPress = (evt) => {
   }
 };
 
+let slideIndex = 1;
+showSlide(slideIndex);
 
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
 
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+};
 
+function showSlide(n) {
+  let i;
+  let slides = document.getElementsByClassName("quotes__card");
+  let dots = document.getElementsByClassName("slider-dot");
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("active", " ");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className+= " active";
+}
 
